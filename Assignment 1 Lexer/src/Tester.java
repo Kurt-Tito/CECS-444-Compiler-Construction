1dd19b5e1e732bd8cc30570eb8a729644a6e91d9
Jimmy Luu <ezico714@gmail.com>
2018-09-25 20:50:28 -0700

    Update Tester.java

--- Assignment 1 Lexer/src/Tester.java
+++ Assignment 1 Lexer/src/Tester.java
@@ -1,14 +1,13 @@
 public class Tester {
     public static void main(String[] args) {
         Lexer input = new Lexer("test.txt");
-
+        
         while (!input.isComplete()) {
-            System.out.printf("%-10s %s\n", input.currentToken(), ("'" +input.Peak() + "'"));
+            System.out.printf("(Tok: %2s %s\n", input.printToken(), ("line= " + input.lineNumber()) + (" str= \"" + input.Peak() + "\")"));
             input.Advance();
-        }
-       if(input.isComplete()) {
-    	   System.out.printf("%-10s %s\n", "TOK0", "'\"\"'");
        }
+       if(input.isComplete()) {
+    	   System.out.printf("%-5s %s\n",  "(Tok:  0 line=", input.lineNumber() + " str= \"\")");
+       }	
     }
 }
-
