f133c0d873977b1bc0f53787a1b2946f5330c3c4
Jimmy Luu <ezico714@gmail.com>
2018-09-28 19:03:29 -0700

    Update Tester.java

--- Assignment 1 Lexer/src/Tester.java
+++ Assignment 1 Lexer/src/Tester.java
@@ -1,18 +1,30 @@
+import java.io.FileNotFoundException;
+
 /**        
  * Tester.java
  * 
  *  This is the main runnable class that displays all the tokens that are processed from the given text file. 
  */
 public class Tester {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Lexer input = new Lexer("test.txt");
         
         while (!input.isComplete()) {
-            System.out.printf("(Tok: %2s %s\n", input.printToken(), ("line= " + input.lineNumber()) + (" str= \"" + input.Peak() + "\")"));
+        	if(input.inputIntCheck(input.Peak())) {
+        		int numInt = Integer.parseInt(input.Peak());
+        		System.out.printf("(Tok: %2s %s\n", input.printToken(), ("line= " + input.lineNumber(input.Peak())) + (" str= \"" + input.Peak() + "\"") + (" int= " + numInt + ")"));
+        	}
+        	else if(input.inputFloatCheck(input.Peak())) {
+        		float numFloat = Float.parseFloat(input.Peak());
+        		System.out.printf("(Tok: %2s %s\n", input.printToken(), ("line= " + input.lineNumber(input.Peak())) + (" str= \"" + input.Peak() + "\"") + (" float= " + numFloat + ")"));
+        	}
+        	else {
+        		System.out.printf("(Tok: %2s %s\n", input.printToken(), ("line= " + input.lineNumber(input.Peak())) + (" str= \"" + input.Peak() + "\")"));
+        	}
             input.Advance();
        }
        if(input.isComplete()) {
-    	   System.out.printf("%-5s %s\n",  "(Tok:  0 line=", input.lineNumber() + " str= \"\")");
+    	   System.out.printf("%-5s %s\n",  "(Tok:  0 line=", input.lineNumber(input.Peak()) + " str= \"\")");
        }	
     }
 }
